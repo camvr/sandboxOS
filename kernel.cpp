6bae0d26c337cf413f85a9aeccbcbfae1cc56bfb
Cam Van Ravens <camvanravens@outlook.com>
2018-01-14 01:42:34 -0500

    Started main kernel and makefile

---
+++ kernel.cpp
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __linux__
+#error "You must be using a cross-compiler!"
+#endif
+
+#ifndef __i386__
+#error "Kernel must be compiled with a ix86-elf compiler!"
+#endif
+
+/* VGA color constants */
+enum vga_color {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
